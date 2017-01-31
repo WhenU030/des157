c2f433bc46ad34a892ee9b8e3a48a99376052257
Wenyu She <wenyu.she@outlook.com>
2017-01-31 11:04:55 -0800

    studio 2 validation improve

--- studio2/script.js
+++ studio2/script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function (event) {
          function process() {
             var adj = document.f.adj.value;
-            var noun = document.f.noun.value;
-            var animal = document.f.animal.value;
-            var noise = document.f.noise.value;
+             var noun = document.f.noun.value;
+             var animal = document.f.animal.value;
+             var noise = document.f.noise.value;
 
             var myMsg = document.getElementById('myMsg');
             myMsg.innerHTML = ('<em>' + adj1 + '</em> Macdonald had a ' + noun + ', E-I-E-I-O. <br> and on that  ' + noun + ' he had an ' + animal + ', E-I-E-I-O <br> with a ' + noise +" " + noise +' here, <br> and a ' + noise +" " + noise +'there, <br>here a ' + noise + 'there a ' + noise + ',<br> everywhere a ' + noise + ', <br>' + adj + ' Macdonald had a ' + noun + ', E-I-E-I-O.');
