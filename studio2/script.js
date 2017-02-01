5dda072be275afce04bfc950ddde7aecccb67b19
Wenyu She <wenyu.she@outlook.com>
2017-01-31 17:33:49 -0800

    studio 2 audio works

--- studio2/script.js
+++ studio2/script.js
@@ -1,6 +1,10 @@
 console.log("Begin");
 document.f.onsubmit = process;
 document.f.onreset = clear;
+// play music when open
+var audio = new Audio('old.mp3');
+audio.play();
+audio.loop = true;
 
 function process(){
   console.log('processing form');
@@ -9,19 +13,19 @@ function process(){
    var userAnimal = document.f.animal.value;
    var userNoise = document.f.noise.value;
    console.log("defined vars");
-   var audio = document.getElementById('music');
-   audio.play();
+   // var audio = document.getElementById('music');
+   // audio.play();
    var myMsg = document.getElementById('results');
-   myMsg.innerHTML = '<em>' + userAdj + '</em> Macdonald had a ' + userNoun + ', E-I-E-I-O. <br> and on that  ' + userNoun + ' he had an ' + userAnimal + ', E-I-E-I-O <br> with a ' + userNoise +" " + userNoise +' here, <br> and a ' + userNoise +" " + userNoise +'there, <br>here a ' + userNoise + 'there a ' + userNoise + ',<br> everywhere a ' + userNoise + ', <br>' + userAdj + ' Macdonald had a ' + userNoun + ', E-I-E-I-O.';
+   myMsg.innerHTML = '<strong>' + userAdj + '</strong> Macdonald had a ' + userNoun + ', E-I-E-I-O. <br> and on that  ' + userNoun + ' he had an ' + userAnimal + ', E-I-E-I-O <br> with a ' + userNoise +" " + userNoise +' here, <br> and a ' + userNoise +" " + userNoise +' there, <br>here a ' + userNoise + ' there a ' + userNoise + ',<br> everywhere a ' + userNoise + ', <br>' + userAdj + ' Macdonald had a ' + userNoun + ', E-I-E-I-O.';
    console.log("showed msg");
    return false;
 }
 
 function clear(){
-    var audio = document.getElementById('music');
+    // var audio = document.getElementById('music');
+    // audio.pause();
+    // audio.currentTime = 0;
     var myMsg = document.getElementById('results');
-    audio.pause();
-    audio.currentTime = 0;
     myMsg.innerHTML = "";
     var rsetInput = document.getElementById('input-f').reset();
     return false;
